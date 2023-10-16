482847f6e18941fa0ce0db01f6363120dc6f94b3
kaytee07 <kofi.taylor07@gmail.com>
2023-10-16 19:28:35 +0000

    search for schools that studey topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+function that returns the list of school having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    list of schools having a specific topic
+    """
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return mongo_collection.find(topic_filter)
